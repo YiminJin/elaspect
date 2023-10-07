8f67e18cd5807d80abdd4367a99877ff747aad48
Yimin Jin <jinym@cea-igp.ac.cn>
2023-10-07 11:35:00 +0800

    add copyrights

--- include/elaspect/boundary_composition/function.h
+++ include/elaspect/boundary_composition/function.h
@@ -1,3 +1,25 @@
+/*
+  Copyright (C) 2023 by Yimin Jin.
+
+  This file is part of elASPECT.
+
+  elASPECT is modified from the free software ASPECT; you can 
+  redistribute it and/or modify it under the terms of the GNU 
+  General Public License as published by the Free Software 
+  Foundation; either version 2, or (at your option) any later 
+  version.
+
+  elASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with elASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef _elaspect_boundary_composition_function_h
 #define _elaspect_boundary_composition_function_h
 
